ac1d5c0b58fcfd531cc2f5bc1b20e828265edf74
Ralf Anton Beier <ralf_beier@me.com>
2023-06-25 09:25:16 +0200

    Migrate functionality to libary
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- myt2abrp/src/lib.rs
+++ myt2abrp/src/lib.rs
@@ -6,17 +6,14 @@ use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
-use tracing::{debug, info};
+use tracing::debug;
 
 /// Send an HTTP request and return the response.
 #[http_component]
 fn send_outbound(_req: Request) -> Result<Response> {
-    let address = Authenticate {
-        username: config::get("username").expect("could not get variable username"),
-        password: config::get("password").expect("could not get variable password"),
-    };
-    let string_address = serde_json::to_string(&address).unwrap();
-    let bytes_address = Some(Bytes::from(string_address));
+    let username = config::get("username").expect("could not get variable username");
+    let password = config::get("password").expect("could not get variable password");
+    let address = Authenticate::new(username, password);
     let request = http::Request::builder()
         .method("POST")
         .uri("https://ssoms.toyota-europe.com/authenticate")
@@ -25,51 +22,49 @@ fn send_outbound(_req: Request) -> Result<Response> {
         .header("X-TME-LC", "de-de")
         .header("Sec-Fetch-Dest", "empty")
         .header("Accept", "application/json, text/plain, */*")
-        .body(bytes_address)?;
+        .body(address.into())?;
     //println!("{:?}", request);
+    let result = spin_sdk::outbound_http::send_request(request).unwrap();
+    if result.status().as_u16() != 200 {
+        println!("Authentication failed");
+        return Ok(Response::new(None));
+    } else {
+        println!("Authentication successful");
+    }
+    let result: AuthenticateResult = result.body().into();
+
+    let status = format!(
+        "https://myt-agg.toyota-europe.com/cma/api/vehicles/{}/remoteControl/status",
+        config::get("vin").expect("could not get variable vin")
+    );
+    let cookie: String = format!("iPlanetDirectoryPro={}", result.token);
+    let request: Request = http::Request::builder()
+        .method("GET")
+        .uri(status)
+        .header("content-type", "application/json")
+        .header("X-TME-APP-VERSION", "4.10.0")
+        .header("X-TME-BRAND", "TOYOTA")
+        .header("X-TME-LOCALE", "de-de")
+        .header("Accept", "application/json, text/plain, */*")
+        .header("Cookie", cookie)
+        .header("UUID", result.customer_profile.uuid)
+        .body(None)?;
     let result = spin_sdk::outbound_http::send_request(request).unwrap();
     if let Some(body) = &result.body().clone() {
         let body = String::from_utf8(body.to_vec()).unwrap();
         debug!("Body: {:?}", body);
-        let result: AuthenticateResult = serde_json::from_str(&body).unwrap();
-        info!("Struct: {:?}", result);
-
-        let status = format!(
-            "https://myt-agg.toyota-europe.com/cma/api/vehicles/{}/remoteControl/status",
-            config::get("vin").expect("could not get variable vin")
+        let remote_control_status: RemoteControlStatus = serde_json::from_str(&body).unwrap();
+        println!(
+            "SOC: {:?}",
+            remote_control_status
+                .vehicle_info
+                .charge_info
+                .charge_remaining_amount
         );
-        let cookie: String = format!("iPlanetDirectoryPro={}", result.token);
-        let request: Request = http::Request::builder()
-            .method("GET")
-            .uri(status)
-            .header("content-type", "application/json")
-            .header("X-TME-APP-VERSION", "4.10.0")
-            .header("X-TME-BRAND", "TOYOTA")
-            .header("X-TME-LOCALE", "de-de")
-            .header("Accept", "application/json, text/plain, */*")
-            .header("Cookie", cookie)
-            .header("UUID", result.customer_profile.uuid)
-            .body(None)?;
-        let result = spin_sdk::outbound_http::send_request(request).unwrap();
-        if let Some(body) = &result.body().clone() {
-            let body = String::from_utf8(body.to_vec()).unwrap();
-            debug!("Body: {:?}", body);
-            let remote_control_status: RemoteControlStatus = serde_json::from_str(&body).unwrap();
-            println!(
-                "SOC: {:?}",
-                remote_control_status
-                    .vehicle_info
-                    .charge_info
-                    .charge_remaining_amount
-            );
-        }
-        //println!("{:?}", result);
-        //let mut res: http::Response<Option<Bytes>> = result;
-        let res = Response::new(Some(Bytes::from("foo")));
-        debug!("{:?}", res);
-        Ok(res)
-    } else {
-        let res = Response::new(Some(Bytes::from("bar")));
-        Ok(res)
     }
+    //println!("{:?}", result);
+    //let mut res: http::Response<Option<Bytes>> = result;
+    let res = Response::new(Some(Bytes::from("foo")));
+    debug!("{:?}", res);
+    Ok(res)
 }
